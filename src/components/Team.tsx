c2f7a2c84af64e1a6dafe58bac977bb52032275b
Efraim Nabil <efraimefefraim@gmail.com>
2024-01-28 21:51:58 +0200

    fix the image problem

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -24,12 +24,12 @@ const Team = ({teamName, fromBox}: IProps) => {
     e.preventDefault();
     setIsDragging(false);
     console.log('drag end');
-    setDropped(true); // this is not working
+    setDropped(true);
     console.log(dropped);
   }
   return (
     <div 
-      className={`flex flex-col items-center py-1 px-2 my-4 mx-2 border ${isDragging ? 'border-green-500' : 'border-gray-400'}`}
+      className={`relative flex flex-col items-center py-1 px-2 my-4 mx-2 border ${isDragging ? 'border-green-500' : 'border-gray-400'}`}
       draggable
       onDragStart={handleDragStart} 
       onDragEnd={handleDragEnd}
@@ -37,7 +37,7 @@ const Team = ({teamName, fromBox}: IProps) => {
       
       <Flag teamName={teamName}/>
       <p className='select-none' draggable="false">{teamName}</p>
-    
+      <div className="absolute top-0 left-0 w-14 h-14 bg-transparent z-5"></div>
     </div>
   )
 }
